df52a964af7dcd8bf0f3e1f0a2672ee4e2c7ca28
bguhu <bozdoganguher@gmail.com>
2021-10-21 21:16:51 +0200

    removing script errors

--- main/demo.py
+++ main/demo.py
@@ -33,12 +33,13 @@ if __name__ == "__main__":
 
     with LivySession.create(url=LIVY_URL, spark_conf=config_dict, 
         # https://pypi.org/project/python-geoip-geolite2 python package
-        py_files=['https://files.pythonhosted.org/packages/df/59/3f611ecca70bc91959e3e1ec325f7835d15cc35585af71dbc6c1123be48e/python-geoip-geolite2-2015.0303.tar.gz'],
+        py_files=['https://files.pythonhosted.org/packages/23/91/ca12b671f11cf774ab851d68770d9a0b2f29b1f1888da8b946faf8d3f2eb/python_geoip-1.2-py27-none-any.whl'],
         #jars=["https://repo1.maven.org/maven2/org/apache/spark/spark-sql-kafka-0-10_2.11/2.4.6/spark-sql-kafka-0-10_2.11-2.4.6.jar",
         #"https://repo1.maven.org/maven2/org/apache/kafka/kafka-clients/2.0.0/kafka-clients-2.0.0.jar"]
     ) as session:
         code= textwrap.dedent(f"""
             from pyspark.sql.functions import from_json, col, to_date, hour, lit
+            from pyspark.sql.functions import udf
             from pyspark.sql.window import Window
             from pyspark.sql.types import *
             from geoip import geolite2
@@ -72,36 +73,45 @@ if __name__ == "__main__":
                 StructField("referer", StringType()),
                 StructField("agent", StringType())])
             
-  
-            # to reach optimal partitioning size of 20-30gb per orc file, we use this window
-            # to add a partitioning column
-            # partition_window = Window().orderBy(lit('1'))
-            # # to filter non valid log lines
+            # to filter non valid log lines
             nonempty_log_line_cond = (col("host").isNotNull() 
                 | col("method").isNotNull() 
                 | col("path").isNotNull() 
                 | col("code").isNotNull() 
                 | col("size").isNotNull()) 
 
-            tmp_df=df.select(
+            tmp_df = df.select(
                     # convert kafka data payload from json to structtype
                     from_json(col("value").cast("string"), schema).alias("payload"),
-                    # select both timestamp and date columns (date is used to partition on daily basis)
+                    # select both timestamp and date columns 
+                    # (date is used to partition on daily basis)
                     col("timestamp"), to_date("timestamp").alias("date"), 
                 ).select(
-                    udf_country_code(col("payload.host")).alias("country_code"), 
+                    col("payload.host").alias("host"), 
                     col("payload.method").alias("method"), 
                     col("payload.path").alias("path"), 
                     col("payload.code").alias("code"), 
                     col("payload.size").alias("size"), 
                     col("date"), col("timestamp"), hour(col("date")).alias("hour")
                 ).filter(nonempty_log_line_cond)
-                
-           
-            result = tmp_df.writeStream.outputMode('append').format("orc")\
+
+            def proc_rdd(iterator):
+                iterator.map(
+                    lambda item : (get_country_code(item._1), item._2, item._3, item._4, item._5, item._6, item._7, item._8)
+                ).toDF(["cc", "method", "path", "code","size","date", "timestamp","hour"]) \
+                .write.outputMode('append').format("orc")\
                 .option("checkpointLocation",  "checkpoints") \
                 .option("path", "/tmp/orc_log_data.orc") \
-                .partitionBy("date", "hour").start()
+                .partitionBy("date", "hour")
+            )
+
+            result = tmp_df.rdd.forEachRDD(proc_rdd)
+                
+           
+            # result = tmp_df.writeStream.outputMode('append').format("orc")\
+            #     .option("checkpointLocation",  "checkpoints") \
+            #     .option("path", "/tmp/orc_log_data.orc") \
+            #     .partitionBy("date", "hour").start()
 
             result.awaitTermination()
             
