b746c24b54e68ff45adabf1980d0ce2241a5533d
bguhu <bozdoganguher@gmail.com>
2021-10-21 07:07:47 +0200

    adding project structure, refactoring code

--- main/demo.py
+++ main/demo.py
@@ -10,24 +10,15 @@ import textwrap
 
 if __name__ == "__main__":
     """
-        To run this Python script you need to install livy-python-api-*version*.tar.gz with
-        easy_install first.
-        python /pathTo/pi_app.py http://<livy-server>:8998 2
+        To run this Python script you need to install requirements.txt first.
+     
     """
 
-    if len(sys.argv) != 3:
-        print("Usage: pi_app <livy url> <slices>", file=sys.stderr)
-        exit(-1)
-
-    slices = int(sys.argv[2])
-    samples = 100000 * slices
-
     config_dict = {
-        'spark.app.name':'Test App',
+        'spark.app.name':'Streaming Log Processor App',
         'spark.jars.packages':'org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.0-preview,org.apache.kafka:kafka-clients:2.3.1',
         'partition.assignment.strategy': 'range',
         'kafka.partition.assignment.strategy':'range'
-
     }
     
 
@@ -63,28 +54,3 @@ if __name__ == "__main__":
     """)
 
         session.run(code)
-        # Run some code on the remote cluster
-        #res=session.download("result")
-        # Retrieve the result
-        #res.show()
-
-
-
-    # # client = HttpClient(sys.argv[1])
-    # client = HttpClient("http://192.168.49.91:8998", load_defaults=False, config_dict=config_dict)
-    
-    
-
-    # def f(_):
-    #     x = random() * 2 - 1
-    #     y = random() * 2 - 1
-    #     return 1 if x ** 2 + y ** 2 <= 1 else 0
-
-    # def pi_job(context):
-    #     count = context.sc.parallelize(range(1, samples + 1), slices).map(f).reduce(add)
-    #     return 4.0 * count / samples
-
-    # pi = client.submit(pi_job).result()
-
-    # print("Pi is roughly %f" % pi)
-    # client.stop(True)
